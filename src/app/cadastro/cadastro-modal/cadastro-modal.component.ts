5a5ec1488ad5fb84fa8b33941ba528824e2d276a
Yuri Moreira <75076180+Yurimrrr@users.noreply.github.com>
2022-10-14 20:19:20 -0300

    Finalizacao cadastro, precisa implementar localstorage para continuar

--- src/app/cadastro/cadastro-modal/cadastro-modal.component.ts
+++ src/app/cadastro/cadastro-modal/cadastro-modal.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { Endereco } from 'src/app/entities/endereco.model';
+import { Usuario } from 'src/app/entities/usuario.model';
 import { UsuarioEntity } from 'src/app/entities/usuarioEntity.model';
 import { UsuarioVan } from 'src/app/entities/usuarioVan.model';
-import { CadastroService } from 'src/app/services/cadastro.service';
+import { Van } from 'src/app/entities/vans.model';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { UsuarioVanService } from 'src/app/services/usuarioVan.service';
 
 @Component({
   selector: 'app-cadastro-modal',
@@ -13,14 +17,19 @@ import { CadastroService } from 'src/app/services/cadastro.service';
 export class CadastroModalComponent implements OnInit {
 
 public usuario: UsuarioEntity = new UsuarioEntity();
-public usuarioVan: UsuarioVan = new UsuarioVan();
 
-  type: boolean;
+public van: Van = new Van();
+
+public endereco: Endereco = new Endereco();
+
+  type: boolean = true;
 
   constructor(
-              private modalCtrl: ModalController, 
-              private rotaAtiva: ActivatedRoute,
-              private rota: Router) { }
+              private modalCtrl: ModalController,
+              // private rotaAtiva: ActivatedRoute,
+              // private rota: Router,
+              private usuarioVanServ: UsuarioVanService,
+              private usuarioServ: UsuarioService) { }
 
   ngOnInit() {}
 
@@ -28,20 +37,44 @@ public usuarioVan: UsuarioVan = new UsuarioVan();
     return this.modalCtrl.dismiss(null, 'cancel');
   }
 
-  public cadastroUsuarioModal(){
-    this.usuario = {
-      id: this.usuario.id,
-      nome: this.usuario.nome,
-      senha: this.usuario.senha,
-      email: this.usuario.email,
-      endereco: this.usuario.endereco,
-      cpf: this.usuario.cpf,
-      foto: this.usuario.foto
+  cadastrar(){
+    //id temporario dps o banco gera
+    let id = Math.floor(Math.random() * 99999999);
+    if(this.type){
+      this.usuario = {
+        id: id,
+        nome: this.usuario.nome,
+        senha: this.usuario.senha,
+        email: this.usuario.email,
+        endereco: this.usuario.endereco,
+        cpf: this.usuario.cpf,
+        foto: "teste"
+      }
+      this.usuarioServ.insertUsuario(this.usuario)
+    }else{
+      let id_van = Math.floor(Math.random() * 99999999);
+      this.van = {
+        id: id_van,
+        nome: this.van.nome,
+        cnpj: this.van.cnpj,
+        bairro: this.van.bairro,
+        descricao: this.van.descricao,
+        foto: "https://img0.icarros.com/dbimg/imgmodelo/2/699_15.png"
+      }
+      let usuarioVan = new UsuarioVan();
+      usuarioVan = {
+        id: id,
+        nome: this.usuario.nome,
+        senha: this.usuario.senha,
+        email: this.usuario.email,
+        endereco: this.usuario.endereco,
+        cpf: this.usuario.cpf,
+        foto: this.usuario.foto,
+        van: this.van
+      }
+      this.usuarioVanServ.insertUsuarioVan(usuarioVan)
     }
   }
-
-
-
 
   confirm(type: boolean) {
     this.type = type;
