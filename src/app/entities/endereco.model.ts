94c9efd0e63f1677a8f899fa9d16ef8e9bff5f7f
Yuri Moreira <yurimoreira557@gmail.com>
2022-10-14 10:17:31 -0300

    Service usuario cadastrar

---
+++ src/app/entities/endereco.model.ts
@@ -0,0 +1,11 @@
+export class Endereco {
+
+    // constructor()
+    constructor(public id?: number,
+                public rua?: string,
+                public bairro?: string,
+                public cidade?: string,
+                public estado?: string,
+                public pais?: string) {
+    }
+}
---
+++ src/app/usuario.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from './entities/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private usuario: Usuario) { }
+
+  usuarios : Array<Usuario>;
+
+  
+  private getAllJogadores(): Array<Usuario> {
+    return this.usuarios;
+  }
+
+  private getUsuarioById(id: number): Usuario{
+    const usuarioEncontrado = this.usuarios.find(obj => obj.id === id);
+
+    if (!usuarioEncontrado) {
+      console.log("Usuario não encontrado")
+    }
+    return usuarioEncontrado;
+  }
+
+  private insertUsuario(usuarioDto: Usuario): void {
+    const { email } = usuarioDto;
+    const usuarioEncontrado = this.usuarios.find(obj => obj.email === email);
+
+    if (usuarioEncontrado) {
+      console.log(`Usuario com o e-mail ${email} já cadastrado`);
+    }
+
+    const jogadorCriado = this.usuarios.push(usuarioDto);
+  }
+
+  private updateUsuario(usuarioDto: Usuario): void{
+
+    let usuarioEncontrado = this.usuarios.find(obj => obj.id === usuarioDto.id);
+
+    if (!usuarioEncontrado) {
+      console.log("Usuario não encontrado")
+    }
+
+    usuarioEncontrado = usuarioDto;
+
+  }
+
+  private deleteJogador(id: number): void {
+    // const usuarioEncontrado = this.usuarios.find(obj => obj.id === id);
+
+    // if (!usuarioEncontrado) {
+    //   console.log("Usuario não encontrado")
+    // }
+
+  }
+}
